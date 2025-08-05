31518e3904217945a16bedf631429b3b838ac5a4
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2025-08-04 22:59:51 -0300

    created account app with login, logout and register

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps
     'learning_logs',
+    'accounts',
         
     # default apps
     'django.contrib.admin',
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# my configs
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
